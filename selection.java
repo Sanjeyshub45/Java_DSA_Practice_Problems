c7476de70a0f377bef2a0a6f383f4000210377a2
sanjey s <sanjeys@sanjeys-MacBook-Air.local>
2025-10-06 22:05:33 +0530

    Added new files

---
+++ selection.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+class selection{
+  public static void main(String args[]){
+
+    int arr[] = {6,5,4,3,2,1};
+    selection(arr);
+    System.out.println(Arrays.toString(arr));
+
+  }
+
+  static void selection(int[] arr){
+
+    for(int i =0;i<arr.length;i++){
+
+       int min = i;
+
+      for(int j =i+1;j<arr.length;j++){
+       
+        if(arr[j]<arr[min]){
+          min = j;
+        }
+
+      }
+       swap(arr,i,min);
+    }
+
+  }
+
+  static void  swap(int[] arr,int first,int last){
+    int temp = arr[last];
+    arr[last] = arr[first];
+    arr[first] = temp;
+  }
+}
