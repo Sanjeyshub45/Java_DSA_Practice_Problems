efaae8bf2a8d2be1370f05deb9bffca85f70e50c
Sanjey S <sanjeys@Sanjeys-MacBook-Air.local>
2025-10-27 19:12:48 +0530

    a

--- cyclicsort.java
+++ cyclicsort.java
@@ -1,39 +1,28 @@
-import java.util.Arrays; 
+import java.util.Arrays;
 
-class cyclicsort{
-  public static void main(String[] args) {
+public class cyclicsort {
 
-    int[] arr = {3,5,2,1,4};
-
-    csort(arr);
-    System.out.println(Arrays.toString(arr));
-        
-  }
-
-  static void csort(int[] arr){
-
-    int i = 0;
-
-    while(i < arr.length){
-
-       int correct = arr[i] - 1;
-
-       if(arr[i]!=arr[correct])
-       {
-        swap(arr,i,correct);
-       }
-       else{
-        i++ ;
-       }
-
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 2, 7, 5, 6, 4};
+        csort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-   
-  }
-  static void swap(int[] arr,int i,int correct){
 
-    int temp = arr[i];
-    arr[i] = arr[correct];
-    arr[correct] = temp;
+    static void csort(int arr[]) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
 
-  }
+    static void swap(int arr[], int from, int to) {
+        int temp = arr[from];
+        arr[from] = arr[to];
+        arr[to] = temp;
+    }
 }
