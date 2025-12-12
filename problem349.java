e6099e36b4b96251cea8505c117622e434af730a
Sanjey S <sanjeys@Sanjeys-MacBook-Air.local>
2025-12-12 15:19:16 +0530

    Added

--- problem349.java
+++ problem349.java
@@ -3,39 +3,45 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class problem349 {
+
     public static void main(String[] args) {
 
-        int[] arr1 = {1,2,2,1};
-        int[] arr2 = {2,2};
-        int[] ans =  intersect(arr1,arr2);
-      System.out.println(Arrays.toString(ans));
+        int[] arr1 = {1,2,4,5,6};
+         int[] arr2= {1,2,4};
+         int[] ans = intersect(arr1,arr2);
+         System.out.println(Arrays.toString(ans));
+
         
     }
+
 
     public static int[] intersect(int[] arr1,int[] arr2){
 
         HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int num:arr1){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-         HashSet<Integer> result = new HashSet<>();
+        for(int i :arr1){
+            map.put(i,map.getOrDefault(i,0)+1);
 
-        for(int num:arr2){
-            if(map.containsKey( num) && map.get(num)>0){
-                result.add(num);
-                map.put(num,map.get(num) - 1);
-            }
         }
-            int[] arr = new int[result.size()];
-           int index = 0;
-           for(int i:result){
-            arr[index++] = i;
-    
-           }
+
+        HashSet<Integer> sol = new HashSet<>();
         
-        return arr;
+        for(int i : arr2){
+            if(map.containsKey( i) && map.get(i)>0){
+                sol.add(i);
+                map.put(i,map.get(i) - 1);
+            }
+  
+        }
+        int[] finalAns = new int[sol.size()];
+        int index = 0;
+        for(int i :sol){
+            finalAns[index++] = i;
+        }
 
+
+return finalAns;
     }
+   
     
 }
