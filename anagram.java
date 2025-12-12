d438a10f2ce60a89a2f1fb919c390b3009bd684a
Sanjey S <sanjeys@Sanjeys-MacBook-Air.local>
2025-12-12 07:10:35 +0530

    ADDED

--- anagram.java
+++ anagram.java
@@ -1,31 +1,35 @@
-public class anagram {
+public class anagram{
+
+    public static void main(String[] args) {
+        String str1 = "cat";
+         String str2 = "aatc";
+
+        boolean ans =  checkAnagram(str1,str2);
+        if(ans){
+            System.out.println("It is anagram");
+
+        }
+        else{
+            System.out.println("Not an anagram");
+        }
 
 
-  public static void main(String[] args) {
-
-
-    String one  = "cat";
-    String two  = "catcat";
-
-    System.out.println(isAnagram(one,two));
-    
-  }
-  public static boolean isAnagram(String one,String two) {
-
-    int[] charArr = new int[26];
-
-    for (int ch : one.toCharArray()) {
-      charArr[ch-'a'] ++ ;    
     }
-    for (int ch : two.toCharArray()) {
-      charArr[ch-'a'] -- ;    
+    public static boolean checkAnagram(String str1,String str2){
+        int[] store = new  int[26];
+        for(char c :str1.toCharArray()){
+            store[c-'a']++;
+        }
+        for(char c :str2.toCharArray()){
+            store[c-'a']--;
+        }
+       for(int count : store){
+        if(count!=0){
+            return false;
+        }
+
+       }
+        return true;
     }
-    for (int ch : charArr) {
-      if(ch!=0){
-        return false;
-      }    
-    }
-    return true;
-  }
-  
+
 }
