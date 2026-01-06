7176698bfb7f892c4f0ef48d822c2e8de0e2b8d0
Sanjey S <sanjeys@Sanjeys-MacBook-Air.local>
2026-01-06 09:43:14 +0530

    Added

--- problem2413.java
+++ problem2413.java
@@ -1,11 +1,11 @@
 public class problem2413 {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         int ans = checkNum(n);
         System.out.println(ans);
     }
     public static int checkNum(int n){
-        return (n%2==0)?n:n/2;
+        return (n%2==0 && n%n==0)? n : n*2;
     }
     
 }
